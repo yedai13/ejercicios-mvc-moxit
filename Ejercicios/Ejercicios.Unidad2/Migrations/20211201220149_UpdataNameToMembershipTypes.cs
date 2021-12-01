5f48f273e339a878bf8ed251118ef8d34053f6d2
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 19:03:10 -0300

    Update names to membership types

---
+++ Ejercicios/Ejercicios.Unidad2/Migrations/20211201220149_UpdataNameToMembershipTypes.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ejercicios.Unidad2.Migrations
+{
+    public partial class UpdataNameToMembershipTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Pay as You Go' WHERE Id =1");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Monthly' WHERE Id =2");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Quarterly' WHERE Id =3");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Annual' WHERE Id =4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
