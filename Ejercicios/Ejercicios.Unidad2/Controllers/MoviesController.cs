407cb5af4f9a409955fe6c750a34b3820a1fe613
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 20:45:13 -0300

    modified moviesController, add details view

--- Ejercicios/Ejercicios.Unidad2/Controllers/MoviesController.cs
+++ Ejercicios/Ejercicios.Unidad2/Controllers/MoviesController.cs
@@ -4,21 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Ejercicios.Unidad2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ejercicios.Unidad2.Controllers
 {
     public class MoviesController : Controller
     {
+        private VidlyDBContext _context;
+
+        public MoviesController(VidlyDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            List<Movie> movies = new()
-            {
-                new Movie {Id = 1, Name = "Toys Story"},
-                new Movie {Id = 2, Name = "Shrek!"},
-                new Movie {Id = 1, Name = "Wall-e"}
-
-            };
+            List<Movie> movies = _context.Movie.Include(m => m.Genre).ToList();
             return View(movies);
+        }
+
+        [Route("movies/details/{id}")]
+        public IActionResult Details(int id)
+        {
+            Movie movie = _context.Movie.Include(m => m.Genre).FirstOrDefault(c => c.Id == id);
+
+            if (movie != null)
+                return View(movie);
+
+            return RedirectToAction("Index");
         }
     }
 }
