70645d2c8a21c818d37dd41d11fe572a3b095708
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 19:18:57 -0300

    add birthdate to customer

--- Ejercicios/Ejercicios.Unidad2/Migrations/20211201221812_AddBirthdateToCustomer.Designer.cs
+++ Ejercicios/Ejercicios.Unidad2/Migrations/20211201221812_AddBirthdateToCustomer.Designer.cs
@@ -4,14 +4,16 @@ using Ejercicios.Unidad2.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ejercicios.Unidad2.Migrations
 {
     [DbContext(typeof(VidlyDBContext))]
-    partial class VidlyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211201221812_AddBirthdateToCustomer")]
+    partial class AddBirthdateToCustomer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ejercicios/Ejercicios.Unidad2/Migrations/20211201221812_AddBirthdateToCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ejercicios.Unidad2.Migrations
+{
+    public partial class AddBirthdateToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "Customer",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "Customer");
+        }
+    }
+}
