03a1736a4fb82e093a9cd5fb3b986205c240288c
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 18:20:14 -0300

    add migration membershipType

---
+++ Ejercicios/Ejercicios.Unidad2/Migrations/20211201211927_AddMembershipType.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ejercicios.Unidad2.Migrations
+{
+    public partial class AddMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "MembershipTypeId",
+                table: "Customer",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.CreateTable(
+                name: "MembershipType",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(type: "tinyint", nullable: false),
+                    SignUpFee = table.Column<short>(type: "smallint", nullable: false),
+                    DurationInMonths = table.Column<byte>(type: "tinyint", nullable: false),
+                    DiscountRate = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembershipType", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_MembershipTypeId",
+                table: "Customer",
+                column: "MembershipTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customer_MembershipType_MembershipTypeId",
+                table: "Customer",
+                column: "MembershipTypeId",
+                principalTable: "MembershipType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customer_MembershipType_MembershipTypeId",
+                table: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "MembershipType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customer_MembershipTypeId",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "MembershipTypeId",
+                table: "Customer");
+        }
+    }
+}
