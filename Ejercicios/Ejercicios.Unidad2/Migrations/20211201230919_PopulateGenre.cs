07c13ccc2b53b5fda574528ff7cc8d695324d812
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 20:13:46 -0300

    add populate genre migration

---
+++ Ejercicios/Ejercicios.Unidad2/Migrations/20211201230919_PopulateGenre.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ejercicios.Unidad2.Migrations
+{
+    public partial class PopulateGenre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (1, 'Action')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (2, 'Comedy')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (3, 'Family')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (4, 'Romance')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (5, 'Horror')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
