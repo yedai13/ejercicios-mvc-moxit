155b1bc4f8950c6808d4da8d60fb7dae7d20697e
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 18:44:57 -0300

    add discont rate in view

--- Ejercicios/Ejercicios.Unidad2/Controllers/CustomersController.cs
+++ Ejercicios/Ejercicios.Unidad2/Controllers/CustomersController.cs
@@ -4,32 +4,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ejercicios.Unidad2.Controllers
 {
     public class CustomersController : Controller
     {
-        // List<Customer> customers = new()
-        // {
-        //     new Customer() {Id = 1, Name = "Facundo", Surname = "Marin"},
-        //     new Customer() {Id = 2, Name = "Willy", Surname = "Smith"},
-        //     new Customer() {Id = 3, Name = "Mary", Surname = "Williams"},
-        //
-        // };
+        private VidlyDBContext _context;
+
+        public CustomersController(VidlyDBContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
-           
-            return View();
+            var customers = _context.Customer.Include(c => c.MembershipType).ToList();
+            return View(customers);
         }
 
         [Route("customers/details/{id}")]
         public IActionResult Details(int id)
         {
-            // Customer customer = customers.Where(c => c.Id == id).FirstOrDefault();
+            Customer customer = _context.Customer.FirstOrDefault(c => c.Id == id);
 
-            // if(customer!=null)
-            //     return View(customer);
+            if(customer!=null)
+                return View(customer);
 
             return RedirectToAction("Index");
         }
