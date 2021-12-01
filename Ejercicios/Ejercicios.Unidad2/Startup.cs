eccbcfb96a8e23d0de4d8151457252694c814302
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 00:47:11 -0300

    agregando controladores

--- Ejercicios/Ejercicios.Unidad2/Startup.cs
+++ Ejercicios/Ejercicios.Unidad2/Startup.cs
@@ -50,7 +50,7 @@ namespace Ejercicios.Unidad2
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Customers}/{action=Index}/{id?}");
             });
         }
     }
