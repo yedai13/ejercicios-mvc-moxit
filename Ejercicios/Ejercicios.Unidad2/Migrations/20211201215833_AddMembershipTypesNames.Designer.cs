191e4e21cffd3dc680a2405693343037c6fafbab
yedai13 <facundo.marin2012@gmail.com>
2021-12-01 19:00:26 -0300

    add membership names

--- Ejercicios/Ejercicios.Unidad2/Migrations/20211201215833_AddMembershipTypesNames.Designer.cs
+++ Ejercicios/Ejercicios.Unidad2/Migrations/20211201215833_AddMembershipTypesNames.Designer.cs
@@ -3,14 +3,16 @@ using Ejercicios.Unidad2.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ejercicios.Unidad2.Migrations
 {
     [DbContext(typeof(VidlyDBContext))]
-    partial class VidlyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211201215833_AddMembershipTypesNames")]
+    partial class AddMembershipTypesNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ejercicios/Ejercicios.Unidad2/Migrations/20211201215833_AddMembershipTypesNames.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ejercicios.Unidad2.Migrations
+{
+    public partial class AddMembershipTypesNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipType",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipType");
+        }
+    }
+}
