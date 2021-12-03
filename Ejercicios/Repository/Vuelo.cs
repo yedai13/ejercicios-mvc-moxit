fc30375bcdf1504f76ee878cd68eaeee0c80aea3
yedai13 <facundo.marin2012@gmail.com>
2021-12-03 20:30:48 -0300

    add repository proyect and model vuelo

---
+++ Ejercicios/Repository/Vuelo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    class Vuelo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string NumeroVuelo { get; set; }
+
+        [Required]
+        public DateTime HorarioLlegada { get; set; }
+
+        [Required]
+        public string LineaAerea { get; set; }
+        public bool Demora { get; set; }
+    }
+}
+
---
+++ Ejercicios/Repository/VuelosDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    class VuelosDbContext : DbContext
+    {
+        public VuelosDbContext(DbContextOptions<VuelosDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Vuelo> Vuelo { get; set; }
+    }
+}
