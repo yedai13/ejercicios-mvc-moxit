98e6ca0664f0e1b87530aa5773379c9471b4ba6f
yedai13 <facundo.marin2012@gmail.com>
2021-12-03 00:59:54 -0300

    add api movieController

---
+++ Ejercicios/Ejercicios.Unidad2/Controllers/Api/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Ejercicios.Unidad2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Ejercicios.Unidad2.Controllers.Api
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private VidlyDBContext _context;
+        private IMapper _mapper;
+
+        public MoviesController(VidlyDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+    }
+}
