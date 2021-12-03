4201c889083bf143be89c151b5af5b97db784a39
yedai13 <facundo.marin2012@gmail.com>
2021-12-03 20:00:01 -0300

    add proyect vuelos

---
+++ Ejercicios/Vuelos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vuelos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
