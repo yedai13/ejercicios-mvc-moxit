f40a87e14c3c9ec5d772630255c18f09db3af11e
yedai13 <facundo.marin2012@gmail.com>
2021-12-04 03:41:53 -0300

    add createNewRental functionality

--- Ejercicios/Ejercicios.Unidad2/Controllers/Api/NewRentalsController.cs
+++ Ejercicios/Ejercicios.Unidad2/Controllers/Api/NewRentalsController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ejercicios.Unidad2.Models;
 
 namespace Ejercicios.Unidad2.Controllers.Api
 {
@@ -12,11 +13,36 @@ namespace Ejercicios.Unidad2.Controllers.Api
     [ApiController]
     public class NewRentalsController : ControllerBase
     {
+        private VidlyDBContext _context;
+
+        public NewRentalsController(VidlyDBContext context)
+        {
+            _context = context; 
+        }
 
         [HttpPost]
         public IActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            return null;
+            var customer = _context.Customer.Single(
+                c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movie.Where(m => newRental.MoviesIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rental.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
