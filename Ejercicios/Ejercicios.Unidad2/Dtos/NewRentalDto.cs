c24e28899b5ef9b4539b6ea80da0e2072f3f8652
yedai13 <facundo.marin2012@gmail.com>
2021-12-04 03:28:13 -0300

    addd NewRentalsController and NewRentalDto

---
+++ Ejercicios/Ejercicios.Unidad2/Dtos/NewRentalDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicios.Unidad2.Dtos
+{
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+        public List<int> MoviesIds { get; set; }
+    }
+}
