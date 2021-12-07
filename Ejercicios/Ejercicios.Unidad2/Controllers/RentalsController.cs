4212d54f8d30edc1e3fead413bfd81699fcfd055
yedai13 <facundo.marin2012@gmail.com>
2021-12-07 15:56:02 -0300

    add new rentals to adminNavbar

--- Ejercicios/Ejercicios.Unidad2/Controllers/RentalsController.cs
+++ Ejercicios/Ejercicios.Unidad2/Controllers/RentalsController.cs
@@ -3,9 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ejercicios.Unidad2.Filter;
 
 namespace Ejercicios.Unidad2.Controllers
 {
+    [IsAdmin]
     public class RentalsController : Controller
     {
         public IActionResult New()
